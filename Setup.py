47e37dd1855c2ef9f620b038786aa84e2adec16b
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2020-08-14 23:44:21 +0530

    first

--- Setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executables("Snakef.py")]
-cx_Freeze.setup(
-    name="The Snake Game",
-	options={"build.exe":{"packages":["pygame"],"include_files":["apple.png","Grass.jpg","Snake1.png","sadsnake.png"]}},
-	description="Snake Game",
-	executables=executables
-	)
--- Snake/Setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("Snakef.py")]
-cx_Freeze.setup(
-    name="The Snake Game",
-	#options={"build.exe":{"packages":["pygame"],"include_files":["images/apple.png","images/Grass.jpg","images/Snake1.png","images/sadsnake.png"]}},
-	description="Snake Game",
-	executables=executables
-	)
